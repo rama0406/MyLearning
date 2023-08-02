e32eec2a8ae8fa71199aed72eb5984bd7ded2824
root <root@ip-172-31-45-62.ap-south-1.compute.internal>
2023-08-02 19:04:37 +0000

    My first task of terraform code

--- main.tf
+++ main.tf
@@ -1,11 +1,103 @@
 provider "aws" {
   region = "ap-south-1"
 }
-resource "aws_instance" "web" {
-  ami           = "ami-006935d9a6773e4ec"
-  instance_type = "t3.micro"
+
+resource "aws_vpc" "test" {
+  cidr_block = "172.16.0.0/16"
 
   tags = {
-    Name = "champ_client"
+    Name = "Project VPC"
   }
 }
+resource "aws_subnet" "public_subnets" {
+  count      = length(var.public_subnet_cidrs)
+  vpc_id     = aws_vpc.test.id
+  cidr_block = element(var.public_subnet_cidrs, count.index)
+
+  tags = {
+    Name = "Public Subnet ${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count      = length(var.private_subnet_cidrs)
+  vpc_id     = aws_vpc.test.id
+  cidr_block = element(var.private_subnet_cidrs, count.index)
+
+  tags = {
+    Name = "Private Subnet ${count.index + 1}"
+  }
+}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.test.id
+
+  tags = {
+    Name = "Project VPC IG"
+  }
+}
+resource "aws_route_table" "test_rt" {
+  vpc_id = aws_vpc.test.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "2nd Route Table"
+  }
+}
+resource "aws_route_table_association" "public_subnet_asso" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.test_rt.id
+}
+resource "aws_instance" "web" {
+  ami           = "ami-006935d9a6773e4ec" #Amazon Linux AMI
+  instance_type = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnets[1].id
+  vpc_security_group_ids      = [aws_security_group.TF_SG.id]
+  associate_public_ip_address = true
+  #first method
+  key_name = "champ"
+  tags = {
+    Name = "Test_demo"
+  }
+}
+
+resource "aws_security_group" "TF_SG" {
+  name        = "security group using Terraform"
+  description = "security group using Terraform"
+  vpc_id      = aws_vpc.test.id
+
+  ingress {
+    description      = "Jenkins"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "TF_SG"
+  }
+}
+
